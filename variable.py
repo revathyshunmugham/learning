453cf90dc2e108be8b1807e41df3f2cf96d056ce
revathyshunmugham <revathys@hexaware.com>
2026-03-05 12:58:43 +0530

    first python script

---
+++ variable.py
@@ -0,0 +1,72 @@
+"""
+name=input("name:")
+age=input("age:")
+address=input("Address")
+print("my name is", name)
+print("my age is", age)
+print(name, " is my name and", age, "is my age")
+print(address)
+"""
+
+"""
+a=int(input("Enter a value"))
+b=int(input("Enter b value"))
+c=int(input("Enter c value"))
+#d=a*b*c
+print("Multiplied values",a*b*c)
+#e=a+b+c
+print("Added values",a+b+c)
+print(a*b*c/10)
+"""
+
+"""
+score=int(input("Enter the score"))
+if(score<35):
+    print("poor")
+elif(score>35 and score<70):
+    print("average")
+else:
+    print("good")
+"""
+
+"""
+score=int(input("Enter the score"))
+if(score>70):
+    input("Enter name")
+    input("enter department")
+    input("enter location")
+    print ("you are eligible")
+else:
+    print("you are not eligible")
+"""
+
+""" FOR LOOP
+a=0
+for i in range(1,11):
+    if(i%2==0):
+        a=a+1
+print(a)
+        """
+""" LIST
+a=[]
+for i in range(1,11):
+    b=int(input("Enter number"))
+    a.append(b)
+print(a)
+"""
+
+
+"""cube
+
+number=int(input("Enter the term"))
+for i in range(1,number+1):
+    print("Number is", str(i), "cube is " ,i*i*i)
+"""
+
+"""WHILE"""
+
+i=10
+while(i<=200):
+    print(i)
+    i=i+10
+    
